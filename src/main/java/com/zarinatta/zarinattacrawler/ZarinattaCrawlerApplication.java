3b0a8e92b68274481a2e1887c20b9ab22e34f47b
JunHyuk Yu <ericyoo0107@gmail.com>
2024-07-25 00:29:20 +0900

    feat: first commit

---
+++ src/main/java/com/zarinatta/zarinattacrawler/ZarinattaCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.zarinatta.zarinattacrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZarinattaCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZarinattaCrawlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zarinatta/zarinattacrawler/ZarinattaCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zarinatta.zarinattacrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZarinattaCrawlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
