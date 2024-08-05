ac329af245a6e0c2e4e5ee1742e25796587dfc95
JunHyuk Yu <ericyoo0107@gmail.com>
2024-08-05 11:08:19 +0900

    feat: 크롤링 요청 할때 마다 페이로드를 바꿔 줘야 함으로 스레드 풀에서 페이로드 추가 로직 제거

--- src/main/java/com/zarinatta/zarinattacrawler/config/HttpClientConfig.java
+++ src/main/java/com/zarinatta/zarinattacrawler/config/HttpClientConfig.java
@@ -1,6 +1,7 @@
 package com.zarinatta.zarinattacrawler.config;
 
 import com.zarinatta.zarinattacrawler.enums.MainStation;
+import com.zarinatta.zarinattacrawler.enums.MainStationCode;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
@@ -37,31 +38,11 @@ public class HttpClientConfig {
     @Bean
     public List<HttpPost> httpPostRequests(CloseableHttpClient httpClient) {
         List<HttpPost> requests = new ArrayList<>();
-        LocalDate today = LocalDate.now();
-        DateTimeFormatter year = DateTimeFormatter.ofPattern("YYYY");
-        DateTimeFormatter month = DateTimeFormatter.ofPattern("MM");
-        DateTimeFormatter day = DateTimeFormatter.ofPattern("dd");
-        DateTimeFormatter week = DateTimeFormatter.ofPattern("EE", Locale.KOREAN);
-        DateTimeFormatter total = DateTimeFormatter.ofPattern("yyyyMMdd");
-
-        for (MainStation depart : MainStation.values()) {
-            for (MainStation arrive : MainStation.values()) {
-                String payload ="txtGoStartCode=&txtGoEndCode=&radJobId=1&selGoTrain=05&txtSeatAttCd_4=015&txtSeatAttCd_3=000&txtSeatAttCd_2=000&txtPsgFlg_2=0&txtPsgFlg_3=0&txtPsgFlg_4=0&txtPsgFlg_5=0&chkCpn=N&selGoSeat1=015&selGoSeat2=&txtPsgCnt1=1&txtPsgCnt2=0&txtGoPage=1" +
-                        "&txtGoAbrdDt="+today.format(total)+"&selGoRoom=&useSeatFlg=&useServiceFlg=&checkStnNm=Y&txtMenuId=11&SeandYo=N&txtGoStartCode2=&txtGoEndCode2=&hidEasyTalk=" +
-                        "&txtGoStart="+URLEncoder.encode(depart.getName(), StandardCharsets.UTF_8)+
-                        "&txtGoEnd="+URLEncoder.encode(arrive.getName(), StandardCharsets.UTF_8)+
-                        "&start=2024.7.30&selGoHour=00&txtGoHour=000000&selGoYear="+today.format(year)+
-                        "&selGoMonth="+today.format(month)+
-                        "&selGoDay="+today.format(day)+
-                        "&txtGoYoil="+URLEncoder.encode(today.format(week), StandardCharsets.UTF_8)+"" +
-                        "&txtPsgFlg_1=1";
-                HttpPost post = new HttpPost("https://www.letskorail.com/ebizprd/EbizPrdTicketPr21111_i1.do");
-                post.setHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-                post.setHeader("User-Agent",  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36");
-                post.setEntity(new StringEntity(payload, ContentType.APPLICATION_FORM_URLENCODED));
-                requests.add(post);
-            }
-        }
+        HttpPost post = new HttpPost("https://www.letskorail.com/ebizprd/EbizPrdTicketPr21111_i1.do");
+        post.setHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        post.setHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36");
+        post.setHeader("Connection:", "keep-alive");
+        requests.add(post);
         return requests;
     }
 }
