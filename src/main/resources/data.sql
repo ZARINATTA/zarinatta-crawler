4308e5e448e6c4028ed8f41b58090adc954e82c2
JunHyuk Yu <ericyoo0107@gmail.com>
2024-08-06 18:03:17 +0900

    chore: 더미 데이터 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,37 +7,33 @@ INSERT INTO users (user_id, phone_number) VALUES
 
 -- Ticket 데이터 삽입
 INSERT INTO ticket (ticket_id, ticket_type, depart_date, depart_time, depart_station, arrive_time, arrive_station, price) VALUES
-                                                                                                                              (8001, '무궁화호 1203', '20240805', '20240805073800', '평택', '20240805084300', '대전', '5900원'),
-                                                                                                                              (8002, 'ITX-새마을 1021', '20240805', '20240805075300', '평택', '20240805084900', '대전', '8800원'),
-                                                                                                                              (8003, '무궁화호 1205', '20240805', '20240805081000', '평택', '20240805091800', '대전', '5900원'),
-                                                                                                                              (8004, '무궁화호 1207', '20240805', '20240805085400', '평택', '20240805102000', '대전', '5900원'),
-                                                                                                                              (8005, 'ITX-마음 9905', '20240805', '20240805091400', '평택', '20240805103800', '대전', '0원'),
-                                                                                                                              (8006, 'ITX-마음 1105', '20240805', '20240805091400', '평택', '20240805103800', '대전', '0원'),
-                                                                                                                              (8007, 'ITX-새마을 1031', '20240805', '20240805095900', '평택', '20240805105500', '대전', '8800원'),
-                                                                                                                              (8008, 'ITX-새마을 1005', '20240805', '20240805111400', '평택', '20240805121300', '대전', '8800원'),
-                                                                                                                              (8009, 'ITX-마음 1107', '20240805', '20240805125400', '평택', '20240805135300', '대전', '0원'),
-                                                                                                                              (8010, 'ITX-마음 9907', '20240805', '20240805125400', '평택', '20240805135300', '대전', '0원'),
-                                                                                                                              (8011, 'ITX-새마을 1007', '20240805', '20240805131000', '평택', '20240805141200', '대전', '8800원'),
-                                                                                                                              (8012, '무궁화호 1209', '20240805', '20240805151000', '평택', '20240805110500', '대전', '5900원'),
-                                                                                                                              (8013, 'ITX-마음 1009', '20240805', '20240805144400', '평택', '20240805154000', '대전', '8800원'),
-                                                                                                                              (8014, '무궁화호 1211', '20240805', '20240805101000', '평택', '20240805160700', '대전', '5900원'),
-                                                                                                                              (8015, '무궁화호 1210', '20240805', '20240805140000', '평택', '20240805164500', '대전', '5900원');
+                                                                                                                              (8001, '무궁화호 1209', '20240815', '20240815130400', '서울', '20240815185900', '부산', '28600원'),
+                                                                                                                              (8002, 'KTX 103', '20240815', '20240815130800', '서울', '20240815162700', '부산', '53500원'),
+                                                                                                                              (8003, 'KTX 033', '20240815', '20240815132500', '서울', '20240815155800', '부산', '59800원'),
+                                                                                                                              (8004, 'KTX 183', '20240815', '20240815134900', '서울', '20240815162400', '부산', '59800원'),
+                                                                                                                              (8005, 'KTX 1009', '20240815', '20240815135300', '서울', '20240815184500', '부산', '42600원'),
+                                                                                                                              (8006, 'KTX 035', '20240815', '20240815135800', '서울', '20240815163400', '부산', '59800원'),
+                                                                                                                              (8007, '무궁화호 1211', '20240815', '20240815140400', '서울', '20240815193900', '부산', '28600원'),
+                                                                                                                              (8008, 'KTX 185', '20240815', '20240815141500', '서울', '20240815164300', '부산', '59800원'),
+                                                                                                                              (8009, 'KTX 037', '20240815', '20240815142800', '서울', '20240815171100', '부산', '59800원'),
+                                                                                                                              (8010, 'KTX 039', '20240815', '20240815145500', '서울', '20240815173800', '부산', '59800원');
 
-INSERT INTO BOOK_MARK (bookmark_id, is_time_out, want_first_class, want_normal_seat, want_baby_seat, waiting_sold_out, ticket_id, user_id) VALUES
-                                                                                                                                               (1, FALSE, 'SOLD_OUT', 'RESERVATION', 'STANDING_SEAT', FALSE, 8001, 1),
-                                                                                                                                               (2, FALSE, 'RESERVATION', 'SOLD_OUT', 'STANDING_SEAT', FALSE, 8001, 2),
-                                                                                                                                               (3, TRUE, 'STANDING_SEAT', 'RESERVATION', 'SOLD_OUT', FALSE, 8001, 3),
-                                                                                                                                               (4, FALSE, 'SOLD_OUT', 'RESERVATION', 'STANDING_SEAT', FALSE, 8001, 1),
-                                                                                                                                               (5, FALSE, 'RESERVATION', 'SOLD_OUT', 'STANDING_SEAT', FALSE, 8001, 2),
-                                                                                                                                               (6, TRUE, 'STANDING_SEAT', 'RESERVATION', 'SOLD_OUT', FALSE, 8001, 3),
-                                                                                                                                               (7, FALSE, 'SOLD_OUT', 'RESERVATION', 'STANDING_SEAT', FALSE, 8001, 1),
-                                                                                                                                               (8, FALSE, 'RESERVATION', 'SOLD_OUT', 'STANDING_SEAT', FALSE, 8001, 2),
-                                                                                                                                               (9, TRUE, 'STANDING_SEAT', 'RESERVATION', 'SOLD_OUT', FALSE, 8002, 3),
-                                                                                                                                               (10, FALSE, 'SOLD_OUT', 'RESERVATION', 'STANDING_SEAT', FALSE, 8002, 1),
-                                                                                                                                               (11, FALSE, 'RESERVATION', 'SOLD_OUT', 'STANDING_SEAT', FALSE, 8002, 2),
-                                                                                                                                               (12, TRUE, 'STANDING_SEAT', 'RESERVATION', 'SOLD_OUT', FALSE, 8002, 3),
-                                                                                                                                               (13, FALSE, 'SOLD_OUT', 'RESERVATION', 'STANDING_SEAT', FALSE, 8002, 1),
-                                                                                                                                               (14, FALSE, 'RESERVATION', 'SOLD_OUT', 'STANDING_SEAT', FALSE, 8002, 2),
-                                                                                                                                               (15, TRUE, 'STANDING_SEAT', 'RESERVATION', 'SOLD_OUT', FALSE, 8002, 3);
+
+INSERT INTO BOOK_MARK (bookmark_id, is_time_out, want_first_class, want_normal_seat, want_baby_seat, want_waiting_reservation, ticket_id, user_id) VALUES
+                                                                                                                                                       (1, FALSE, TRUE, 'SEAT', 'STANDING_SEAT', FALSE, 8001, 1),
+                                                                                                                                                       (2, FALSE, FALSE, 'STANDING_SEAT', 'SEAT', FALSE, 8001, 2),
+                                                                                                                                                       (3, TRUE, TRUE, 'NOTFOUND', 'SEAT', FALSE, 8001, 3),
+                                                                                                                                                       (4, FALSE, TRUE, 'SEAT', 'STANDING_SEAT', FALSE, 8002, 1),
+                                                                                                                                                       (5, FALSE, FALSE, 'STANDING_SEAT', 'SEAT', FALSE, 8002, 2),
+                                                                                                                                                       (6, TRUE, TRUE, 'NOTFOUND', 'SEAT', FALSE, 8002, 3),
+                                                                                                                                                       (7, FALSE, TRUE, 'SEAT', 'STANDING_SEAT', FALSE, 8003, 1),
+                                                                                                                                                       (8, FALSE, FALSE, 'STANDING_SEAT', 'SEAT', FALSE, 8003, 2),
+                                                                                                                                                       (9, TRUE, TRUE, 'NOTFOUND', 'SEAT', FALSE, 8003, 3),
+                                                                                                                                                       (10, FALSE, TRUE, 'SEAT', 'STANDING_SEAT', FALSE, 8004, 1),
+                                                                                                                                                       (11, FALSE, FALSE, 'STANDING_SEAT', 'SEAT', FALSE, 8004, 2),
+                                                                                                                                                       (12, TRUE, TRUE, 'NOTFOUND', 'SEAT', FALSE, 8004, 3),
+                                                                                                                                                       (13, FALSE, TRUE, 'SEAT', 'STANDING_SEAT', FALSE, 8005, 1),
+                                                                                                                                                       (14, FALSE, FALSE, 'STANDING_SEAT', 'SEAT', FALSE, 8005, 2),
+                                                                                                                                                       (15, TRUE, TRUE, 'NOTFOUND', 'SEAT', FALSE, 8005, 3);
 
 
