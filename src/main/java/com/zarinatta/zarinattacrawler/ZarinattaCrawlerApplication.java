698a2f867ec35ebdebf76f1e0cfede9288829aa5
JunHyuk Yu <ericyoo0107@gmail.com>
2025-02-04 00:11:39 +0900

    feat: 외부 api 호출에 Spring retry 적용

--- src/main/java/com/zarinatta/zarinattacrawler/ZarinattaCrawlerApplication.java
+++ src/main/java/com/zarinatta/zarinattacrawler/ZarinattaCrawlerApplication.java
@@ -2,10 +2,8 @@ package com.zarinatta.zarinattacrawler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableRetry
 @EnableScheduling
 @SpringBootApplication
 public class ZarinattaCrawlerApplication {
