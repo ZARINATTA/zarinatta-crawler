6a3ddae0d82320fb4fe5bda21f4d67f4ae1efb2c
JunHyuk Yu <ericyoo0107@gmail.com>
2025-09-09 21:09:04 +0900

    refactor: 티켓 repository단 상속 구조 변경

--- src/main/java/com/zarinatta/zarinattacrawler/service/api/InitialDataSetting.java
+++ src/main/java/com/zarinatta/zarinattacrawler/service/api/InitialDataSetting.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zarinatta.zarinattacrawler.entity.Ticket;
 import com.zarinatta.zarinattacrawler.enums.StationCode;
-import com.zarinatta.zarinattacrawler.repository.TicketRepositoryCustom;
+import com.zarinatta.zarinattacrawler.repository.TicketRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
@@ -32,7 +32,7 @@ import java.util.concurrent.Executors;
 public class InitialDataSetting implements CommandLineRunner {
 
     private final ApiService apiService;
-    private final TicketRepositoryCustom ticketRepository;
+    private final TicketRepository ticketRepository;
     private final String requestUrl = "http://apis.data.go.kr/1613000/TrainInfoService/getStrtpntAlocFndTrainInfo";
     private final String serviceKey = "HfhAs61GSdPS9xgGhAlNLbH0YlnRdtbNa7MZVlJ6dAN5r7e3AYePUE9nQZv7X0PDqltq3o6ljr%2BKkLWb5TNzjg%3D%3D";
     private final ExecutorService executorService = Executors.newFixedThreadPool(30);
