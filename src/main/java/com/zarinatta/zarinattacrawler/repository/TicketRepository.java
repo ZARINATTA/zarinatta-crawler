73ed767d6e72c413f4bf2faf0621caf939db06a3
JunHyuk Yu <ericyoo0107@gmail.com>
2026-02-13 01:39:25 +0900

    feat: 10일전 열차는 삭제하는 스케쥴러 개발

--- src/main/java/com/zarinatta/zarinattacrawler/repository/TicketRepository.java
+++ src/main/java/com/zarinatta/zarinattacrawler/repository/TicketRepository.java
@@ -1,7 +1,9 @@
 package com.zarinatta.zarinattacrawler.repository;
 
 import com.zarinatta.zarinattacrawler.entity.Ticket;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -13,4 +15,11 @@ public interface TicketRepository extends JpaRepository<Ticket, Long>, TicketRep
 
     @Query("SELECT DISTINCT t FROM Ticket t JOIN FETCH t.bookMarks b JOIN FETCH b.user u WHERE t.id IN :ticketIds ORDER BY t.id")
     List<Ticket> findTicketsWithDetailsByIds(@Param("ticketIds") List<Long> ticketIds);
+
+    @Query("SELECT t.id FROM Ticket t WHERE t.departDate < :targetDate")
+    List<Long> findIdsByDepartDateBefore(@Param("targetDate") String targetDate, Pageable pageable);
+
+    @Modifying(clearAutomatically = true)
+    @Query("DELETE FROM Ticket t WHERE t.id IN :ids")
+    void deleteByIdIn(@Param("ids") List<Long> ids);
 }
